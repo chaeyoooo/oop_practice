8145aafceb63989c613223a5f5340b5c47a1a17b
Chaeyeoncho <gcy9293@naver.com>
2025-04-11 02:13:10 +0900

    refactoring : file directory

--- src/baekjoon/FourBasicOperation/Add.java
+++ src/baekjoon/FourBasicOperation/Add.java
@@ -1,14 +1,14 @@
-package baekjoon;
+package baekjoon.FourBasicOperation;
 
 import java.util.Scanner;
 
-public class Subtract {
+public class Add {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
         int A = sc.nextInt();
         int B = sc.nextInt();
 
-        System.out.println(A-B);
+        System.out.println(A+B);
     }
 }
--- src/baekjoon/FourBasicOperation/HelloWorld.java
+++ src/baekjoon/FourBasicOperation/HelloWorld.java
@@ -1,4 +1,4 @@
-package baekjoon;
+package baekjoon.FourBasicOperation;
 
 public class HelloWorld {
     public static void main(String[] args){
